2b50c7c7680e771524145f8833ead8e6ee87c1a0
nori Seokju Yun <noricube@gmail.com>
2016-08-18 17:46:17 +0900

    Config 클래스 추가 - ini 파싱 데이터 저장 클래스

---
+++ InstallerForQurare/Installer/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketBeatInstaller
+{
+    class Config
+    {
+        public int Port { get; set; }
+        public string Logstash { get; set; }
+    }
+}
